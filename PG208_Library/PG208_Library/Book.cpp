3dc8d8dc833b5a563b89f623299b739af2d7c289
Mr.Fab <mr.fab@live.com>
2016-04-17 15:52:58 +0200

    Signed-off-by: Mr.Fab <mr.fab@live.com>

---
+++ PG208_Library/PG208_Library/Book.cpp
@@ -0,0 +1,49 @@
+#include "StdAfx.h"
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <fstream>
+
+#include "Book.h"
+
+using namespace std;
+
+Book::Book()
+{
+    _author = "NA";
+    _publisher = "NA";
+    _pages = 0;
+}
+
+
+std::string Book::getAuthor()
+{return _author;}
+
+void Book::setAuthor(std::string newAuthor)
+{_author = newAuthor;}
+
+
+std::string Book::getPublisher()
+{return _publisher;}
+
+void Book::setPublisher(std::string newPublisher)
+{_publisher = newPublisher;}
+
+
+int Book::getPages()
+{return _pages;}
+
+void Book::setPages(int newPages)
+{_pages = newPages;}
+
+void Book::getData()
+{
+	getGeneralData();
+	
+	cout << "Author: " << getAuthor() << endl;
+	cout << "Publisher: " << getPublisher() << endl;
+	cout << "Pages: " << getPages() << endl;
+		
+	cout << "----------------------------------------" << endl;
+}
+
