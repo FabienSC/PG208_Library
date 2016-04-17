e9790cec6b7336fb6e04f127beccf3858f1107c2
Alex MacHuggon <Alex MacHuggon>
2016-04-17 15:40:01 +0200

    lolz2
    Signed-off-by: Alex MacHuggon <Alex MacHuggon>

---
+++ PG208_Library/PG208_Library/Article.h
@@ -0,0 +1,49 @@
+#pragma once
+ref class Article
+{
+public:
+	Article(void);
+};
+
+/*
+#ifndef ARTICLES_H
+#define ARTICLES_H
+#include<string>
+
+
+class Article
+{
+public:
+    Article();
+    ~Article();
+
+    int 		getID();								//1 to 1000 for books, 1001 to 2000 for CDs ...
+    void 		setID(int newID);
+	
+	void 		setTitle(std::string newTitle);		
+    std::string getTitle();						
+    
+	void 		setReleaseDate(int newReleaseDate);		
+    int 		getReleaseDate();						
+    
+	bool 		getAvailability();
+    bool 		borrowArticle();						// if already borrowed, can't borrow it...
+    bool 		returnArticle();						// if in library, can't return...
+
+	void 		getGeneralData();
+
+private:
+    unsigned int _ID;
+    std::string _title;
+    bool _isAvailable;
+    int _releaseDate;//YYYYMMDD
+};
+
+
+
+
+
+
+
+#endif // ARTICLES_H
+*/
